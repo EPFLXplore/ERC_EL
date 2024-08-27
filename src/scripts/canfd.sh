b780e97042ac254145d731257b673f3f82e68511
fhimmiche <126008605+fhimmiche@users.noreply.github.com>
2024-08-27 13:08:11 +0200

    Correct canfd.sh

--- src/scripts/canfd.sh
+++ src/scripts/canfd.sh
@@ -28,8 +28,8 @@ sudo modprobe mttcan
 sudo ip link set down can0
 sudo ip link set down can1
 
-sudo ip link set can0 type can bitrate 500000 sample-point 0.875 dbitrate 2000000 dsample-point 0.9 berr-reporting on one-shot on fd on restart-ms 100
-sudo ip link set can1 type can bitrate 500000 sample-point 0.875 dbitrate 2000000 dsample-point 0.9 berr-reporting on one-shot on fd on restart-ms 100
+sudo ip link set can0 up type can bitrate 500000 sample-point 0.875 dbitrate 2000000 dsample-point 0.9 berr-reporting on fd on restart-ms 100
+sudo ip link set can0 up type can bitrate 500000 sample-point 0.875 dbitrate 2000000 dsample-point 0.9 berr-reporting on fd on restart-ms 100
 
 sudo ifconfig can0 txqueuelen 1000
 sudo ifconfig can1 txqueuelen 1000
